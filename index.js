5836003f098521853ddb23a170734016fdf1c9ec
Sergio Vázquez <sergiovazag@gmail.com>
2020-03-04 16:58:21 -0600

    *Added* Express, Hola mundo en express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola")
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res, next) => {
+    res.send('Bienvenido al servidor');
+})
+
+app.get('/:name', (req, res, next) =>{
+    console.log(req.params.name);
+    res.status(200);
+    res.send('Hola ' + req.params.name);
+});
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('Server is running...');
+});
+
+
