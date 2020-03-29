754bbcef442fe4daa851c22bbe5f1d41b722f6e5
Sergio Vazquez <sergiovazag@gmail.com>
2020-03-29 15:55:36 -0600

    #Added#

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const { pokemon } = require('./pokedex.json');
 
 app.get("/", (req, res, next) => {
-    res.send('Bienvenido al servidor');
-})
-
-app.get('/:name', (req, res, next) =>{
-    console.log(req.params.name);
     res.status(200);
-    res.send('Hola ' + req.params.name);
+    res.send("Bienvenido a pokedex");
+});
+
+app.get('/pokemon/all', (req, res, next) =>{
+    res.status(200);
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:id([0-9]{1,3})',   (req, res, next) =>{
+    const id = req.params.id - 1
+    if (id >= 0 && id <= 150) {
+        res.status(200);
+        res.send(pokemon[id]);
+    }else{
+        res.status(404);
+        res.send('Pokémon no encontrado')
+    }
+});
+
+app.get('/pokemon/:name', (req,res,next) =>{
+    const name = req.params.name;
+    for ( i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].name == name) {
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send('Pokémon no encontrado');
 });
 
 app.listen(process.env.PORT || 3000, () =>{
