beedaffa52a7b56d07283a5145f9006320ce7051
Sergio Vazquez <sergiovazag@gmail.com>
2020-05-24 15:17:59 -0500

    **Added** Signin & getToken

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const notFound = require('./middleware/notFound');
 const index = require('./middleware/index');
 const cors = require('./middleware/cors');
 
-app.user(cors);
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
